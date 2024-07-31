0801e7fab541f7ca2713aede6a06042797597f82
HeinHtet14120 <heinhtet14120@gmail.com>
2024-07-31 16:03:14 +0700

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Highlights from "./components/Highlights"
 import Howitworks from "./components/Howitworks";
 import Model from "./components/Model"
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 import * as Sentry from '@sentry/react';
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
     <Model/>
     <Features/>
     <Howitworks/>
+    <Footer/>
    </main>
   )
 }
